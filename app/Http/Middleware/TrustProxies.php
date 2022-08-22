95267182e0cc0d9c759cefc52dc7461513771537
dani <nghokend@gmail.com>
2022-08-22 18:49:54 +0100

    trust all proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = ["nyannonce.com","64.225.51.118"];
+    protected $proxies = "*";
 
     /**
      * The headers that should be used to detect proxies.
