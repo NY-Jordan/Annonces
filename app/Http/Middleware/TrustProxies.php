fcf075f14d190d2656b9468271c42b17c6f70f33
dani <nghokend@gmail.com>
2022-08-22 18:48:18 +0100

    add trusted proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = ["*","172.17.0.1"];
+    protected $proxies = ["nyannonce.com","64.225.51.118"];
 
     /**
      * The headers that should be used to detect proxies.
